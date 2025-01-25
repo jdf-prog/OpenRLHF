201d19229020a35b02965a8a97d7e528440a304b
wyettzeng <wyettzeng@gmail.com>
2025-01-25 21:22:58 +0000

    update

--- scripts/train_ppo_ray.sh
+++ scripts/train_ppo_ray.sh
@@ -1,47 +1,54 @@
-set -x
-
+set -x 
 working_dir=$PWD
-# reinforce++
-
+# ppo
 ray job submit --address="http://127.0.0.1:8265" \
    --runtime-env-json='{"working_dir": "'$working_dir'"}' \
    -- python3 -m openrlhf.cli.train_ppo_ray \
    --ref_num_nodes 1 \
-   --ref_num_gpus_per_node 1 \
+   --ref_num_gpus_per_node 2 \
    --reward_num_nodes 1 \
-   --reward_num_gpus_per_node 1 \
+   --reward_num_gpus_per_node 2 \
+   --critic_num_nodes 1 \
+   --critic_num_gpus_per_node 2 \
    --actor_num_nodes 1 \
-   --actor_num_gpus_per_node 4 \
-   --vllm_num_engines 1 \
-   --vllm_tensor_parallel_size 1 \
+   --actor_num_gpus_per_node 2 \
+   --vllm_num_engines 2 \
+   --vllm_tensor_parallel_size 2 \
+   --colocate_critic_reward \
+   --colocate_actor_ref \
    --pretrain Qwen/Qwen2.5-7B-Instruct \
    --reward_pretrain CodeDPO/qwen_coder_2.5_rm_openrlhf \
    --value_head_prefix "score" \
-   --save_path $working_dir/examples/test_scripts/checkpoint/qwen25-ins-7b-coderm-7b-reinforce++ \
-   --micro_train_batch_size 16 \
+   --save_path $working_dir/examples/test_scripts/checkpoint/qwen25-ins-7b-coderm-7b-ppo \
+   --micro_train_batch_size 8 \
    --train_batch_size 128 \
    --micro_rollout_batch_size 32 \
-   --rollout_batch_size 256 \
-   --n_samples_per_prompt 8 \
+   --rollout_batch_size 1024 \
+   --n_samples_per_prompt 4 \
+   --max_samples 1000000 \
    --max_epochs 1 \
    --prompt_max_len 2048 \
-   --max_samples 1000000 \
    --generate_max_len 2048 \
-   --advantage_estimator reinforce \
    --zero_stage 3 \
    --bf16 \
    --actor_learning_rate 5e-7 \
+   --critic_learning_rate 9e-6 \
    --init_kl_coef 0.01 \
    --prompt_data CodeDPO/codedpo_20241208_openrlhf_format_hard \
    --input_key context_messages \
    --apply_chat_template \
+   --normalize_reward \
+   --packing_samples \
    --adam_offload \
    --gradient_checkpointing \
-   --packing_samples \
+   --load_checkpoint \
    --save_steps 10 \
    --ckpt_path $working_dir/examples/test_scripts/ckpt/qwen25-ins-7b-coderm-7b-reinforce++ \
    --flash_attn \
    --use_wandb $WANDB_API_KEY \
 
-   # --normalize_reward \
-# also supports --advantage_estimator rloo
+# --runtime-env-json='{"setup_commands": ["pip install openrlhf[vllm]"]}' [Install deps]
+# --ref_reward_offload [Offload to CPU]
+# --remote_rm_url http://localhost:5000/get_reward
+
+# --vllm_sync_backend nccl (Only for multi-nodes with vLLM 0.6.4+ or vLLM 0.4.2)
